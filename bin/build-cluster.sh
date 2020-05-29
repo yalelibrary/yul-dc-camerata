9ca0f7eb6a4c43209793b4a42a6463b5473836c5
Mark Bussey <mark@curationexperts.com>
2020-05-29 11:08:20 -0500

    EFS mounting WIP

--- bin/build-cluster.sh
+++ bin/build-cluster.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -ex
+
 if [[ -z $1 ]]
 then
   echo "ERROR: Please supply a cluster name"
@@ -27,7 +29,7 @@ then
   echo "      AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}";
 #  ecs-cli compose --project-name ${1}-project --file docker-compose-simple.yml --ecs-params ${1}-ecs-params.yml service up --cluster ${1}
   ecs-cli up --cluster ${1} --launch-type FARGATE --region $AWS_DEFAULT_REGION | tee cluster-ids.txt
-  
+
   echo "Extracting vpc & subnet IDs"
   VPC_ID=`sed -n 's/VPC created: \(.*\)/\1/p' < cluster-ids.txt`
   SUBNET0=`grep -Eo "subnet-\w+$" cluster-ids.txt | sed -n '1p'`
@@ -35,7 +37,7 @@ then
   echo "  $VPC_ID"
   echo "  $SUBNET0"
   echo "  $SUBNET1"
-  
+
   echo "Setup ingress security group"
   SG_ID=`aws ec2 describe-security-groups --filters \
     Name=vpc-id,Values=$VPC_ID --region=$AWS_DEFAULT_REGION | grep -Eo -m 1 'sg-\w+'`
@@ -56,15 +58,31 @@ then
     --protocol tcp --port 8182 \
     --cidr 0.0.0.0/0 \
     --region=$AWS_DEFAULT_REGION
-  
+
+
+  SOLR_FS_ID=`aws efs create-file-system \
+    --creation-token ${1}-solr-efs \
+    --performance-mode generalPurpose \
+    --throughput-mode bursting \
+    --region $AWS_DEFAULT_REGION \
+    --tags Key=Name,Value="${1}-solr" \
+      | grep -Eo -m 1 'fs-\w+'`
+
+
   cat <<ECS_PARAMS > ${1}-ecs-params.yml
 version: 1
 task_definition:
   task_execution_role: ecsTaskExecutionRole
   ecs_network_mode: awsvpc
   task_size:
-    mem_limit: 4GB
-    cpu_limit: 512
+    mem_limit: 8GB
+    cpu_limit: 2048
+  efs_volumes:
+      - name: "solr_efs"
+        filesystem_id: $SOLR_FS_ID
+        root_directory: /
+        transit_encryption: DISABLED
+        iam: DISABLE
 run_params:
   network_configuration:
     awsvpc_configuration:
@@ -75,6 +93,6 @@ run_params:
         - $SG_ID
       assign_public_ip: ENABLED
 ECS_PARAMS
-  
+
 fi
 
