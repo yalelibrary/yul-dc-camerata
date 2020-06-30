298d891bcf0e59d962d56964e5c1cc7be593aaa1
Rachel M. Kadel <rachelkg@gmail.com>
2020-06-30 11:00:00 -0400

    Run deploy tests against clusters without verifiable certificates

--- spec/deploy_spec.rb
+++ spec/deploy_spec.rb
@@ -22,22 +22,41 @@ puts "Current Blacklight basic auth settings: " \
      "\n username: #{username}" \
      "\n password: #{password}"
 
-# Checks for cluster urls in ENV
-# Sets to local development defaults if none are found
-blacklight_url = ENV['BLACKLIGHT_URL'] || 'http://localhost:3000'
-iiif_manifest_url = ENV['IIIF_MANIFEST_URL'] || 'http://localhost:80'
-iiif_image_url = ENV['IIIF_IMAGE_URL'] || 'http://localhost:8182'
+# Checked for a deployed cluster host in the environment
+if ENV['YUL_DC_SERVER']
+  blacklight_url = "https://#{ENV['YUL_DC_SERVER']}"
+  iiif_manifest_url = "https://#{ENV['YUL_DC_SERVER']}"
+  iiif_image_url = "https://#{ENV['YUL_DC_SERVER']}"
+else
+  # Checks for cluster urls in ENV
+  # Sets to local development defaults if none are found
+  blacklight_url = ENV['BLACKLIGHT_URL'] || 'http://localhost:3000'
+  iiif_manifest_url = ENV['IIIF_MANIFEST_URL'] || 'http://localhost:80'
+  iiif_image_url = ENV['IIIF_IMAGE_URL'] || 'http://localhost:8182'
+end
+
+# use this SSLContext to use https URLs without verifying certificates
+#@ssl_context = OpenSSL::SSL::SSLContext.new
+#@ssl_context.verify_mode = OpenSSL::SSL::VERIFY_NONE
+ssl_context = OpenSSL::SSL::SSLContext.new
+ssl_context.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+def load_page(uri)
+  HTTP.basic_auth(user: username, pass: password).get(uri)
+end
 
 RSpec.describe "The cluster at #{blacklight_url}" do
   describe "The blacklight site at #{blacklight_url}" do
     let(:uri) { "#{blacklight_url}/" }
     it 'accepts the provided HTTP_PASSWORD and HTTP_USERNAME' do
-      response = HTTP.basic_auth(user: username, pass: password).get(uri)
+      response = HTTP.basic_auth(user: username,
+        pass: password).get(uri, ssl_context:ssl_context)
       expect(response.code).to eq(200)
     end
     it 'loads home page with a language facet present', :ci_pending do
       # pending 'no data found'
-      response = HTTP.basic_auth(user: username, pass: password).get(uri)
+      response = HTTP.basic_auth(user: username,
+        pass: password).get(uri, ssl_context:ssl_context)
       expect(response.code).to eq(200)
       expect(response.body.to_s).to match(/blacklight-language_ssim/)
     end
@@ -45,7 +64,8 @@ RSpec.describe "The cluster at #{blacklight_url}" do
       let(:uri) { "#{blacklight_url}/?search_field=all_fields&q=" }
       it 'with at least 5 pages', :ci_pending do
         # pending 'no data found'
-        response = HTTP.basic_auth(user: username, pass: password).get(uri)
+        response = HTTP.basic_auth(user: username,
+          pass: password).get(uri, ssl_context:ssl_context)
         expect(response.code).to eq(200)
         expect(response.body.to_s).to match(/aria-label="Go to page 5"/)
       end
@@ -54,16 +74,17 @@ RSpec.describe "The cluster at #{blacklight_url}" do
       let(:uri) { "#{blacklight_url}/catalog/16189097" }
       it 'that shows Universal Viewer', :ci_pending do
         # pending 'record not found'
-        response = HTTP.basic_auth(user: username, pass: password).get(uri)
+        response = HTTP.basic_auth(user: username,
+          pass: password).get(uri, ssl_context:ssl_context)
         expect(response.code).to eq(200)
         expect(response.body.to_s).to match(/universal-viewer-iframe/)
       end
     end
     describe 'has a yale-only item' do
       let(:uri) { URI("#{blacklight_url}/catalog/16189097-yale") }
-      it 'that does not show Universal Viewer', :ci_pending do
-        # pending 'record not found'
-        response = HTTP.basic_auth(user: username, pass: password).get(uri)
+      it 'that does not show Universal Viewer' do
+        response = HTTP.basic_auth(user: username,
+          pass: password).get(uri, ssl_context:ssl_context)
         expect(response.code).to eq(200)
         expect(response.body.to_s).not_to match(/universal-viewer-iframe/)
       end
@@ -75,16 +96,16 @@ RSpec.describe "The cluster at #{blacklight_url}" do
     describe 'provides a manifest for item 16686591' do
       let(:oid) { '16685691' }
       it 'serves a manifest for item 16685691 with a sequence containing one canvas' do
-        response = HTTP.get(uri)
-        expect(response.code).to eq(200)
+        response = HTTP.basic_auth(user: username,
+          pass: password).get(uri, ssl_context:ssl_context)
         expect(JSON.parse(response.body)['sequences'][0]['canvases'].length).to eq(1)
       end
     end
     describe 'provides a manifest for item 16856582' do
       let(:oid) { '16854582' }
       it 'has a sequence with nine canvases' do
-        response = HTTP.get(uri)
-        expect(response.code).to eq(200)
+        response = HTTP.basic_auth(user: username,
+          pass: password).get(uri, ssl_context:ssl_context)
         expect(JSON.parse(response.body)['sequences'][0]['canvases'].length).to eq(9)
       end
     end
@@ -95,7 +116,8 @@ RSpec.describe "The cluster at #{blacklight_url}" do
     let(:oid) { '16854589' }
     it 'serves an info.json for image 16854589 that has a width/height ratio between 0.75 and 0.8', :ci_pending do
       # pending "needs AWS credentials"
-      response = HTTP.get(uri)
+      response = HTTP.basic_auth(user: username,
+        pass: password).get(uri, ssl_context:ssl_context)
       expect(response.code).to eq(200)
       parsed = JSON.parse(response.body)
       expect(parsed['width'].to_f / parsed['height'].to_f).to be_between(0.75, 0.8).inclusive
