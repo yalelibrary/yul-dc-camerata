c1955e0c80748c4cae32b0fac85c5e3051ccbbe7
Rachel M. Kadel <rachelkg@gmail.com>
2020-07-10 17:24:27 -0400

    Eliminate manual get-params step from deploy process

--- bin/deploy-solr.sh
+++ bin/deploy-solr.sh
@@ -4,13 +4,16 @@
 . $(dirname "$0")/efs-fun.sh
 export CLUSTER_NAME=${1}
 
-if check_profile && check_region && check_cluster $1 && check_params $1 && check_secrets && all_pass
+if check_profile && check_region && check_cluster $1 && check_secrets && all_pass
 then
   echo "Target cluster: ${CLUSTER_NAME}"
   echo "Using AWS_PROFILE=${AWS_PROFILE}";
   echo "      AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}";
 
-
+  if [[ ! -f ${1}-solr-params.yml ]]
+  then
+    bin/get-params.sh ${1}
+  fi
 
   # Launch the service and register containers with the loadbalancer
   # The $2 here can be anything, but is usually --enable-service-discovery
