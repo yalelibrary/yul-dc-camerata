02a35d6ce174a92f28f04c634559ad7c1fe23a15
Rob Kaufman <rob@notch8.com>
2020-08-13 10:03:10 -0700

    solr warning and halt if still running

---
+++ bin/stop-solr.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+. $(dirname "$0")/shared-checks.sh
+. $(dirname "$0")/efs-fun.sh
+export CLUSTER_NAME=${1}
+
+if check_profile && check_region && check_cluster $1 && check_secrets && all_pass
+then
+  echo "Target cluster: ${CLUSTER_NAME}"
+  echo "Using AWS_PROFILE=${AWS_PROFILE}";
+  echo "      AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}";
+fi
+
+ecs-cli compose  \
+        --region $AWS_DEFAULT_REGION \
+        --project-name ${CLUSTER_NAME}-solr \
+        service stop -c ${CLUSTER_NAME}
