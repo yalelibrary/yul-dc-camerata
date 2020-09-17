ac35c1df1c5a0e292463582a1126b3ffbf2b2da7
Bess Sadler <bess@curationexperts.com>
2020-09-17 11:28:57 -0400

    Refactor camerata release process (#187)
    * Refactor camerata release process
    
    Use the release notes to determine how to increment the version number, since the
    github_changelog_generator gem has already written all the logic for debugging PRs vs
    commits vs releases vs timestamps.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,5 +24,6 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
+  config.ignore_localhost = true
   config.allow_http_connections_when_no_cassette = true
 end
