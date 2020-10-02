072c21842c2790dd1bd54a22d901bf74983bd303
Keith Carter <kcarter241@gmail.com>
2020-10-02 09:54:53 -0400

    add jenkins deploy script

---
+++ jenkins/deploy.sh
@@ -0,0 +1,75 @@
+/usr/bin/env bash
+
+export PATH=$PATH:/usr/local/bin
+export AWS_PROFILE=default
+export AWS_DEFAULT_REGION=us-east-1
+
+declare -A CLUSTER_URL
+CLUSTER_URL=( ["yul-dc-test"]="collections-test.library.yale.edu"
+              ["yul-dc-uat"]="collections-uat.library.yale.edu"
+              ["yul-dc-demo"]="collections-demo.library.yale.edu" )
+
+gem install bundler
+
+#./bin/setup
+
+bundle install --quiet
+
+#BR commented per R Kadel Copy secrets_template to secrets
+#cp secrets-template ./.secrets
+
+#sed -i 's/^HTTP_USERNAME/HTTP_USERNAME=yul-dc/g' .secrets
+#sed -i 's/^HTTP_PASSWORD/HTTP_PASSWORD=nocrawl/g' .secrets
+
+#install camerata gem
+rake install
+
+CAMERATA_VERSION=`cam version | awk {'print "v"$3}'`
+
+#Retrieve service definitions from AWS ECS Cluster
+VPC_ID=vpc-57bee630 SUBNET0=subnet-2dc03400 SUBNET1=subnet-71b55b4d cam get-params $CLUSTER_NAME
+
+
+#Push application versions to AWS SSM
+if [ "$UPDATE_SSM" = "true" ]
+then
+	echo "UPDATING AWS SSM"
+    if [ ! "$BLACKLIGHT_VERSION" = '' ]
+    then
+    	cam push_version blacklight $BLACKLIGHT_VERSION
+    fi
+
+    if [ ! "$MANAGEMENT_VERSION" = '' ]
+    then
+		cam push_version management $MANAGEMENT_VERSION
+    fi
+
+    if [ ! "$IIIF_MANIFEST_VERSION" = '' ]
+    then
+    	cam push_version iiif_manifest $IIIF_MANIFEST_VERSION
+    fi
+
+    if [ ! "$IIIF_IMAGE_VERSION" = '' ]
+    then
+    	cam push_version iiif_image $IIIF_IMAGE_VERSION
+    fi
+
+    cam push_version camerata $CAMERATA_VERSION
+fi
+
+echo "UPDATE_SSM -> ${UPDATE_SSM}"
+echo "BLACKLIGHT -> ${BLACKLIGHT_VERSION}"
+echo "MANAGEMENT -> ${MANAGEMENT_VERSION}"
+echo "IIIF_MANIFEST -> ${IIIF_MANIFEST_VERSION}"
+echo "IIIF_IMAGE -> ${IIIF_IMAGE_VERSION}"
+echo "CAMERATA -> ${CAMERATA_VERSION}"
+
+#Update containers from Git
+cam deploy-main $CLUSTER_NAME
+cam deploy-worker $CLUSTER_NAME
+
+sleep 10
+
+# Run smoke tests after deployment
+YUL_DC_SERVER=${CLUSTER_URL["$CLUSTER_NAME"]} cam smoke
+
