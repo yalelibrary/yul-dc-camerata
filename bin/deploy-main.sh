712e2c1f3a837224d4c617c228840ffc742ca5b2
Keith Carter <kcarter241@gmail.com>
2020-10-09 13:20:15 -0400

    remove commit line change

--- bin/deploy-main.sh
+++ bin/deploy-main.sh
@@ -44,8 +44,8 @@ then
   echo $MGMT_TG_ARN
 
   # Launch the service and register containers with the loadbalancer
-  ecs-cli compose  \
   # The $2 here can be anything, but is usually --enable-service-discovery
+  ecs-cli compose  \
     --region $AWS_DEFAULT_REGION \
     --project-name ${CLUSTER_NAME}-main \
     --ecs-params ${CLUSTER_NAME}-ecs-params.yml \
