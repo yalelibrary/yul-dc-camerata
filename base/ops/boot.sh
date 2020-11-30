4df339413befe304fc313817db4ecb9791a6b702
McClain Looney <m@loonsoft.com>
2020-11-30 13:19:42 -0600

    Add $TYPE variable
    Used to fill out --host param for dynatrace visibility

--- base/ops/boot.sh
+++ base/ops/boot.sh
@@ -3,5 +3,5 @@
 if [ ! -z "$DYNATRACE_TOKEN" ];then
 
   curl -Ls -H "Authorization: Api-Token ${DYNATRACE_TOKEN}" 'https://nhd42358.live.dynatrace.com/api/v1/deployment/installer/agent/unix/default/latest?arch=x86&flavor=default' > installer.sh && \
-  /bin/sh installer.sh  --set-app-log-content-access=true --set-infra-only=true --set-host-group=DC --set-host-name=${CLUSTER_NAME}-blacklight NON_ROOT_MODE=0 &
+  /bin/sh installer.sh  --set-app-log-content-access=true --set-infra-only=true --set-host-group=DC --set-host-name=${CLUSTER_NAME}-${TYPE} NON_ROOT_MODE=0 &
 fi
