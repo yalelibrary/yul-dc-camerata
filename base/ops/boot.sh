d25cdbb63d63bd15c9bd18919d855d6fe32d495f
McClain Looney <m@loonsoft.com>
2020-11-30 15:40:18 -0600

    stupid hack to template nginx

--- base/ops/boot.sh
+++ base/ops/boot.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 #This script is run by /sbin/my_init at boot time
+#this replaces the string @@image_url@@ in the webapp config with the value of $IIIF_IMAGE_BASE_URL
+#before nginx starts
+sed -i "s]@@image_url@@]${IIIF_IMAGE_BASE_URL}]g" /etc/nginx/sites-enabled/webapp.conf
 if [ ! -z "$DYNATRACE_TOKEN" ];then
 
   curl -Ls -H "Authorization: Api-Token ${DYNATRACE_TOKEN}" 'https://nhd42358.live.dynatrace.com/api/v1/deployment/installer/agent/unix/default/latest?arch=x86&flavor=default' > installer.sh && \
