c415ea1f8733598c34936863eb892e1a9d427208
McClain Looney <mlooney@users.noreply.github.com>
2020-11-30 14:00:17 -0600

    Dynatrace2 (#272)
    * Updates for runtime dynatrace support
    
    * Bump version
    
    * Add $TYPE variable
    
    Used to fill out --host param for dynatrace visibility
    
    * Update boot.sh

---
+++ base/ops/boot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+#This script is run by /sbin/my_init at boot time
+if [ ! -z "$DYNATRACE_TOKEN" ];then
+
+  curl -Ls -H "Authorization: Api-Token ${DYNATRACE_TOKEN}" 'https://nhd42358.live.dynatrace.com/api/v1/deployment/installer/agent/unix/default/latest?arch=x86&flavor=default' > installer.sh && \
+  /bin/sh installer.sh  --set-app-log-content-access=true --set-infra-only=true --set-host-group=DC --set-host-name=${CLUSTER_NAME}-${TYPE} NON_ROOT_MODE=0 &
+fi
--- lib/camerata/version.rb
+++ lib/camerata/version.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 module Camerata
-  VERSION = '3.2.0'
+  VERSION = '3.2.1'
 end
