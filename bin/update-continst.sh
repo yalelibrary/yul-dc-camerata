fea6d346df09c6a856b4820907c26b1142da9839
bobrice <robert.rice@yale.edu>
2022-12-05 11:53:21 -0500

    added flex clone mounts (#354)
    * added flex clone mounts
    
    * removed file
    
    * renamed updatecontish.sh

--- bin/update-continst.sh
+++ bin/update-continst.sh
@@ -114,16 +114,26 @@ CLUSTER_NAME=$1
   mkdir -p /brbl-dsu/reshoots_additions
   mkdir -p /yul
 
-  if [ $CLUSTER_NAME == "yul-dc-test" ] || [ $CLUSTER_NAME == "yul-dc-infra" ]
+  if [ $CLUSTER_NAME == "yul-dc-test" ] 
   then
+    FLEXCLONE_SUFFIX="_test_11212022"
     GOOBI_HOT="wcsfs00.its.yale.internal:/NFS_SFS_std_sngl_003/Goobi_Deposits-CC1741-BRBLDSU"
     GOOBI_NON_BRBL="wcsfs00.its.yale.internal:/NFS_SFS_std_sngl_007/yul_goobi_non_BRBL_tst-CC1702-LIBITS"
 
     mount -t nfs -orw,nolock,rsize=32768,wsize=32768,intr,noatime,nfsvers=3 \$GOOBI_NON_BRBL /yul
     mount -t nfs -orw,nolock,rsize=32768,wsize=32768,intr,noatime,nfsvers=3 \$GOOBI_HOT/jss_export /brbl-dsu/jss_export
     mount -t nfs -orw,nolock,rsize=32768,wsize=32768,intr,noatime,nfsvers=3 \$GOOBI_HOT/dcs /brbl-dsu/dcs
+   elif [ $CLUSTER_NAME == "yul-dc-demo" ] 
+   then
+    FLEXCLONE_SUFFIX="_test_11212022"
+    GOOBI_HOT="wcsfs00.its.yale.internal:/NFS_SFS_std_mult_000/Goobi_Deposits_UAT-CC1741-BRBLDSU"
+    GOOBI_NON_BRBL="wcsfs00.its.yale.internal:/NFS_SFS_std_sngl_007/yul_goobi_non_BRBL_uat-CC1702-LIBITS"
+
+    mount -t nfs -orw,nolock,rsize=32768,wsize=32768,intr,noatime,nfsvers=3 \$GOOBI_NON_BRBL /yul
+    mount -t nfs -orw,nolock,rsize=32768,wsize=32768,intr,noatime,nfsvers=3 \$GOOBI_HOT/jss_export /brbl-dsu/jss_export
+    mount -t nfs -orw,nolock,rsize=32768,wsize=32768,intr,noatime,nfsvers=3 \$GOOBI_HOT/dcs /brbl-dsu/dcs
     mount -t nfs -orw,nolock,rsize=32768,wsize=32768,intr,noatime,nfsvers=3 \$GOOBI_HOT/reshoots_additions /brbl-dsu/reshoots_additions
-  elif [ $CLUSTER_NAME == "yul-dc-uat" ] || [ $CLUSTER_NAME == "yul-dc-demo" ]
+  elif [ $CLUSTER_NAME == "yul-dc-uat" ] 
   then
     GOOBI_HOT="wcsfs00.its.yale.internal:/NFS_SFS_std_mult_000/Goobi_Deposits_UAT-CC1741-BRBLDSU"
     GOOBI_NON_BRBL="wcsfs00.its.yale.internal:/NFS_SFS_std_sngl_007/yul_goobi_non_BRBL_uat-CC1702-LIBITS"
@@ -147,7 +157,7 @@ CLUSTER_NAME=$1
   do
     t=\`printf '%02d' \$i\`
     mkdir -p /data/\$t
-    mount -t nfs -orw,nolock,rsize=32768,wsize=32768,intr,noatime,nfsvers=3 wcsfs00.its.yale.internal:/yul_dc_nfs_store_\$i /data/\$t
+    mount -t nfs -orw,nolock,rsize=32768,wsize=32768,intr,noatime,nfsvers=3 wcsfs00.its.yale.internal:/yul_dc_nfs_store_\$i\$FLEXCLONE_SUFFIX /data/\$t
   done" | base64 -w0)
 
   # create launch json template
