f55e8ef7794e36a5bef2dc87952357641a98a6ae
K8Sewell <kait@notch8.com>
2024-10-18 11:53:14 -0500

    Add tests for YCO

--- smoke_spec/deploy_spec.rb
+++ smoke_spec/deploy_spec.rb
@@ -11,12 +11,12 @@ if File.exist?('.secrets')
   secrets = File.read('.secrets').split
 
   secrets.each do |v|
-    username = v.split("=")[1] if v.split("=")[0] == 'HTTP_USERNAME'
-    password = v.split("=")[1] if v.split("=")[0] == 'HTTP_PASSWORD'
+    username = v.split('=')[1] if v.split('=')[0] == 'HTTP_USERNAME'
+    password = v.split('=')[1] if v.split('=')[0] == 'HTTP_PASSWORD'
   end
 end
 
-puts "Current Blacklight basic auth settings: " \
+puts 'Current Blacklight basic auth settings: ' \
      "\n username: #{username}" \
      "\n password: #{password}"
 
@@ -46,8 +46,17 @@ RSpec.describe "The cluster at #{blacklight_url}", type: :feature do
   let(:public_child_oid) { '1030368' }
   let(:yco_parent_oid) { '2043304' }
   let(:yco_child_oid) { '1191792' }
+  let(:owp_parent_oid) { '20433333304' }
+  let(:owp_child_oid) { '1191333333792' }
+  # TODO - set up full text objects to be present in local, test, uat, and prod
+  let(:public_fulltext_parent_oid) { '2005512' }
+  let(:public_fulltext_child_oid) { '1030368' }
+  let(:yco_fulltext_parent_oid) { '2043304' }
+  let(:yco_fulltext_child_oid) { '1191792' }
   let(:deployed_public_parent_oid) { '16371253' }
   let(:deployed_public_child_oid) { '16394803' }
+  let(:deployed_yco_parent_oid) { '2007967' }
+  let(:deployed_yco_child_oid) { '1041543' }
 
   describe "The blacklight site at #{blacklight_url}" do
     let(:uri) { "#{blacklight_url}/catalog/" }
@@ -77,31 +86,23 @@ RSpec.describe "The cluster at #{blacklight_url}", type: :feature do
         expect(response.code).to eq(200)
       end
       describe 'has a public item' do
-        let(:uri) { "#{blacklight_url}/catalog/#{public_parent_oid}" }
         it 'that shows Universal Viewer' do
-          visit uri
+          visit "#{blacklight_url}/catalog/#{public_parent_oid}"
           expect(page).to have_selector('.show-header')
           expect(page).to have_selector('.universal-viewer-iframe')
         end
       end
       describe 'has a yale-only item' do
-        let(:uri) { "#{blacklight_url}/catalog/#{yco_parent_oid}" }
         it 'that does not show Universal Viewer' do
-          visit uri
+          visit "#{blacklight_url}/catalog/#{yco_parent_oid}"
           expect(page).to have_selector('.show-header')
           expect(page).not_to have_selector('.universal-viewer-iframe')
         end
       end
       describe "The manifest service at #{iiif_manifest_url}" do
-        let(:blacklight) { "#{blacklight_url}/catalog/#{oid}" }
-        let(:uri) { "#{iiif_manifest_url}/manifests/#{oid}\.json" }
-        # before do
-        #   visit blacklight
-        # end
-        describe 'provides a manifest in any environment' do
-          let(:oid) { public_parent_oid }
-          it "for Public item and links to the manifest" do
-            visit blacklight
+        describe 'can provide a manifest in any environment' do
+          it 'for Public item and links to the manifest' do
+            visit "#{blacklight_url}/catalog/#{public_parent_oid}"
             expect(page).to have_selector('#manifestLink')
             # Use HTTP rather than visit to avoid getting HTML on our json
             response = HTTP.basic_auth(user: username,
@@ -111,10 +112,30 @@ RSpec.describe "The cluster at #{blacklight_url}", type: :feature do
             expect(JSON.parse(response.body)['items'].length).to eq(2),
               'sequence contains two canvases'
           end
+          it 'but not for YCO items and will not have link' do
+            uri = "#{blacklight_url}/manifests/#{yco_parent_oid}\.json"
+            visit "#{blacklight_url}/catalog/#{yco_parent_oid}"
+            expect(page).to have_selector('#manifestLink'), visible: false
+            # Use HTTP rather than visit to avoid getting HTML on our json
+            response = HTTP.basic_auth(user: username, pass: password)
+                           .get(uri, ssl_context: ssl_context)
+            expect(JSON.parse(response.body)).to eq({'error' => 'unauthorized'}),
+              'no manifest or manifest link'
+          end
+          xit 'except for OWP items and will not have link' do
+            uri = "#{blacklight_url}/manifests/#{owp_parent_oid}\.json"
+            visit "#{blacklight_url}/catalog/#{owp_parent_oid}"
+            expect(page).to have_selector('#manifestLink'), visible: false
+            # Use HTTP rather than visit to avoid getting HTML on our json
+            response = HTTP.basic_auth(user: username, pass: password)
+                           .get(uri, ssl_context: ssl_context)
+            expect(JSON.parse(response.body)).to eq({'error' => 'unauthorized'}),
+              'no manifest or manifest link'
+          end
         end
-        describe 'provides a manifest in deployed environment', deployed: true do
-          let(:oid) { deployed_public_parent_oid }
-          it "for Public item and links to the manifest" do
+        describe 'can provide a manifest in deployed environment', deployed: true do
+          it 'for Public item and links to the manifest' do
+            uri = "#{iiif_manifest_url}/manifests/#{deployed_public_parent_oid}\.json"
             response = HTTP.basic_auth(user: username,
                                        pass: password).get(uri, ssl_context: ssl_context)
             parsed_manifest = JSON.parse(response.body)
@@ -123,34 +144,163 @@ RSpec.describe "The cluster at #{blacklight_url}", type: :feature do
             response = HTTP.basic_auth(user: username,
                                        pass: password)
                            .get(image_uri, ssl_context: ssl_context)
-            expect(response.code).to eq 200
+            expect(response.code).to eq 200, 'image retrievable'
           end
-        end
-        describe 'does not provide a manifest in any environment' do
-          let(:oid) { yco_parent_oid }
-          it "for YCO item and does not link a manifest" do
-            response = HTTP.basic_auth(user: username,
-                                       pass: password).get(uri, ssl_context: ssl_context)
-            parsed_manifest = JSON.parse(response.body)
-            byebug
-            expect(parsed_manifest['items'].length).to eq(6), 'sequence contains six canvases'
-            image_uri = parsed_manifest['items'][0]['items'][0]['items'][0]['body']['id']
+          it 'except for YCO items and will not have link' do
+            uri = "#{iiif_manifest_url}/manifests/#{deployed_yco_parent_oid}\.json"
+            expect(page).not_to have_selector('#manifestLink'), 'no manifest link'
+            # Use HTTP rather than visit to avoid getting HTML on our json
             response = HTTP.basic_auth(user: username,
                                        pass: password)
-                           .get(image_uri, ssl_context: ssl_context)
-            expect(response.code).to eq 200
+                           .get(uri,
+                          ssl_context: ssl_context)
+            expect(JSON.parse(response.body)).to eq({'error' => 'unauthorized'}),
+              'no manifest'
           end
-        end        
+          xit 'except for OWP items and will not have link' do
+            uri = "#{iiif_manifest_url}/manifests/#{deployed_owp_parent_oid}\.json"
+            expect(page).not_to have_selector('#manifestLink'), 'no manifest link'
+            # Use HTTP rather than visit to avoid getting HTML on our json
+            response = HTTP.basic_auth(user: username,
+                                       pass: password)
+                           .get(uri,
+                          ssl_context: ssl_context)
+            expect(JSON.parse(response.body)).to eq({'error' => 'unauthorized'}),
+              'no manifest'
+          end
+        end
       end
       describe "The iiif service at #{iiif_image_url}" do
-        let(:uri) { "#{iiif_image_url}/iiif/2/#{oid}/info.json" }
-        let(:oid) { public_child_oid } 
-        it "serves an info.json for Public image that has a width/height ratio between 1.5 and 1.7" do
+        describe 'info.json' do
+          it 'serves an info.json for Public image that has a width/height ratio between 1.5 and 1.7' do
+            uri = "#{iiif_image_url}/iiif/2/#{public_child_oid}/info.json"
+            response = HTTP.basic_auth(user: username,
+                                      pass: password).get(uri, ssl_context: ssl_context)
+            expect(response.code).to eq(200)
+            parsed = JSON.parse(response.body)
+            expect(parsed['width'].to_f / parsed['height'].to_f).to be_between(1.5, 1.7).inclusive
+          end
+        end
+        describe 'default.jpg' do
+          it 'serves a jpg for Public image' do
+            uri = "#{iiif_image_url}/iiif/2/#{public_child_oid}/full/!200,200/0/default.jpg"
+            response = HTTP.basic_auth(user: username,
+                                      pass: password).get(uri, ssl_context: ssl_context)
+            expect(response.code).to eq(200)
+            expect(response.mime_type).to eq 'image/jpeg'
+            expect(response['Content-Disposition']).to eq("inline; filename=\"1030368.jpg\"")
+          end
+          it 'does not serve a jpg for YCO image' do
+            uri = "#{iiif_image_url}/iiif/2/#{yco_child_oid}/full/!200,200/0/default.jpg"
+            response = HTTP.basic_auth(user: username,
+                                      pass: password).get(uri, ssl_context: ssl_context)
+            expect(response.code).to eq(404)
+            expect(response.mime_type).to eq 'text/plain'
+          end
+          xit 'does not serve a jpg for OWP image' do
+            uri = "#{iiif_image_url}/iiif/2/#{owp_child_oid}/full/!200,200/0/default.jpg"
+            response = HTTP.basic_auth(user: username,
+                      pass: password).get(uri, ssl_context: ssl_context)
+            expect(response.code).to eq(404)
+            expect(response.mime_type).to eq 'text/plain'
+          end          
+        end
+      end
+      describe 'annotations' do
+        # TODO - mark back as functional when fulltext objects are set up
+        xit 'serves an annotation for Public image' do
+          uri = "#{blacklight_url}/annotation/oid/#{public_fulltext_parent_oid}/canvas/#{public_fulltext_child_oid}/fulltext?oid=#{public_fulltext_parent_oid}&child_oid=#{public_fulltext_child_oid}"
           response = HTTP.basic_auth(user: username,
-                                     pass: password).get(uri, ssl_context: ssl_context)
+                                    pass: password).get(uri, ssl_context: ssl_context)
           expect(response.code).to eq(200)
-          parsed = JSON.parse(response.body)
-          expect(parsed['width'].to_f / parsed['height'].to_f).to be_between(1.5, 1.7).inclusive
+          expect(response.body).to eq 'page'
+        end
+        xit 'does not serve an annotation for YCO image' do
+          uri = "#{blacklight_url}/annotation/oid/#{yco_fulltext_parent_oid}/canvas/#{yco_fulltext_child_oid}/fulltext?oid=#{yco_fulltext_parent_oid}&child_oid=#{yco_fulltext_child_oid}"
+          response = HTTP.basic_auth(user: username,
+                                    pass: password).get(uri, ssl_context: ssl_context)
+          expect(response.code).to eq(401), 'has unauthorized response'
+          expect(JSON.parse(response.body)).to eq({'error' => 'unauthorized'})
+        end
+        xit 'does not serve an annotation for OWP image' do
+          uri = "#{blacklight_url}/annotation/oid/#{owp_fulltext_parent_oid}/canvas/#{owp_fulltext_child_oid}/fulltext?oid=#{owp_fulltext_parent_oid}&child_oid=#{owp_fulltext_child_oid}"
+          response = HTTP.basic_auth(user: username,
+                    pass: password).get(uri, ssl_context: ssl_context)
+          expect(response.code).to eq(401), 'has unauthorized response'
+          expect(JSON.parse(response.body)).to eq({'error' => 'unauthorized'})
+        end          
+      end
+      # needs deployed because of connection to management
+      describe 'tiff download', deployed: true do
+        it 'for Public image' do
+          action_uri = "#{blacklight_url}/download/tiff/#{public_child_oid}/staged"
+          retrieval_uri = "#{blacklight_url}/download/tiff/#{public_child_oid}"
+          response = HTTP.basic_auth(user: username,
+                    pass: password).get(action_uri, ssl_context: ssl_context)
+          expect(response.code).to eq(200), 'has success response'
+          response = HTTP.basic_auth(user: username,
+                    pass: password).get(retrieval_uri, ssl_context: ssl_context)
+          expect(response.mime_type).to eq 'image/jpeg'
+          expect(response['Content-Disposition']).to eq("inline; filename=\"1030368.jpg\""), 'serves a tiff'
+        end
+        it 'for YCO image' do
+          action_uri = "#{blacklight_url}/download/tiff/#{yco_child_oid}/staged"
+          retrieval_uri = "#{blacklight_url}/download/tiff/#{yco_child_oid}"
+          response = HTTP.basic_auth(user: username,
+                    pass: password).get(action_uri, ssl_context: ssl_context)
+          expect(response.code).to eq(401), 'has unauthorized response'
+          response = HTTP.basic_auth(user: username,
+                    pass: password).get(retrieval_uri, ssl_context: ssl_context)
+          expect(response.mime_type).to eq 'text/plain', 'does not serve a tiff'
+        end
+        xit 'for OWP image' do
+          action_uri = "#{blacklight_url}/download/tiff/#{owp_child_oid}/staged"
+          retrieval_uri = "#{blacklight_url}/download/tiff/#{owp_child_oid}"
+          response = HTTP.basic_auth(user: username,
+                    pass: password).get(action_uri, ssl_context: ssl_context)
+          expect(response.code).to eq(401), 'has unauthorized response'
+          response = HTTP.basic_auth(user: username,
+                    pass: password).get(retrieval_uri, ssl_context: ssl_context)
+          expect(response.mime_type).to eq 'text/plain', 'does not serve a tiff'
+        end
+      end
+      describe 'pdfs' do
+        it 'serves a pdf for Public image' do
+          response = HTTP.basic_auth(user: username,
+                    pass: password).get("#{blacklight_url}/pdfs/#{public_parent_oid}.pdf", ssl_context: ssl_context)
+          expect(response.code).to eq(200)
+          expect(response.mime_type).to eq 'application/pdf'
+        end
+        it 'does not serve a pdf for YCO image' do
+          response = HTTP.basic_auth(user: username,
+                    pass: password).get("#{blacklight_url}/pdfs/#{yco_parent_oid}.pdf", ssl_context: ssl_context)
+          expect(response.code).to eq(401), 'has unauthorized response'
+          expect(response.mime_type).to eq 'application/json'
+        end
+        xit 'does not serve a pdf for OWP image' do
+          response = HTTP.basic_auth(user: username,
+                    pass: password).get("#{blacklight_url}/pdfs/#{owp_parent_oid}.pdf", ssl_context: ssl_context)
+          expect(response.code).to eq(401), 'has unauthorized response'
+          expect(response.mime_type).to eq 'application/json'
+        end
+      end
+      describe 'owp request service' do
+        let(:request_uri) { "#{blacklight_url}/catalog/#{owp_parent_oid}/request_form" }
+        let(:terms_uri) { "#{blacklight_url}/catalog/#{owp_parent_oid}/terms_and_conditions" }
+        let(:confirmation_uri) { "#{blacklight_url}/catalog/#{owp_parent_oid}/request_confirmation" }
+        xit 'will not be accessible' do
+          response = HTTP.basic_auth(user: username,
+                    pass: password).get(request_uri, ssl_context: ssl_context)
+          expect(response.code).to eq(401), 'has unauthorized response'
+          expect(JSON.parse(response.body)).to eq({'error' => 'unauthorized'}), 'request form'
+          response = HTTP.basic_auth(user: username,
+                    pass: password).get(terms_uri, ssl_context: ssl_context)
+          expect(response.code).to eq(401), 'has unauthorized response'
+          expect(JSON.parse(response.body)).to eq({'error' => 'unauthorized'}), 'terms and conditions'
+          response = HTTP.basic_auth(user: username,
+                    pass: password).get(confirmation_uri, ssl_context: ssl_context)
+          expect(response.code).to eq(401), 'has unauthorized response'
+          expect(JSON.parse(response.body)).to eq({'error' => 'unauthorized'}), 'request confirmation'
         end
       end
     end
@@ -178,26 +328,29 @@ RSpec.describe "The cluster at #{blacklight_url}", type: :feature do
           visit "#{management_url}/api/download/stage/child/#{public_child_oid}"
           expect(page.body.include?('staged for download')).to eq(true), '/api/download/stage/child/:child_oid'
         end
-        it 'will restrict access to YCO' do
+        it 'will not restrict access to YCO' do
           visit "#{management_url}/api/download/stage/child/#{yco_child_oid}"
-          expect(page.body.include?('error')).to eq(true), '/api/download/stage/child/:child_oid'
+          expect(page.body.include?('staged for download')).to eq(true), '/api/download/stage/child/:child_oid'
+          # currently no user permission checks on this action - this will need updated once the permission checks are updated
+          # expect(page.body.include?('error')).to eq(true), '/api/download/stage/child/:child_oid'
         end
+        # enable test and update ids when OWP objects are present in production
         xit 'will restrict access to OWP' do
           visit "#{management_url}/api/download/stage/child/9999999"
           expect(page.body.include?('error')).to eq(true), '/api/download/stage/child/:child_oid'
-          uri = "#{management_url}/api/permission_requests"
+          permission_requests_url = "#{management_url}/api/permission_requests"
           request = JSON.parse({})
-          response = HTTP.post(uri, ssl_context: ssl_context, params: JSON.pretty_generate(request))
+          response = HTTP.post(permission_requests_url, ssl_context: ssl_context, params: JSON.pretty_generate(request))
           expect(response.code).to eq(403), '/api/permission_requests'
           visit "#{management_url}/api/permission_sets/444444-8888-2849239023"
           expect(page.body.include?('error')).to eq(true), '/api/permission_sets/:sub'
-          uri = "#{management_url}/agreement_term"
+          agreement_term_url = "#{management_url}/agreement_term"
           request = JSON.parse({})
-          response = HTTP.post(uri, ssl_context: ssl_context, params: JSON.pretty_generate(request))
+          response = HTTP.post(agreement_term_url, ssl_context: ssl_context, params: JSON.pretty_generate(request))
           expect(response.code).to eq(403), '/api/permission_requests'
         end
         it 'will restrict access to' do
-          visit "#{management_url}/api/user/"
+          visit "#{management_url}/api/user/13"
           expect(page.body.include?('error')).to eq(true), '/api/user'
         end
       end
