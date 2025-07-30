5e2e6a1ec0bf911ba112c6ab7d72561339db0358
Kait Sewell <36549923+K8Sewell@users.noreply.github.com>
2025-07-30 14:56:32 -0500

    Version addition (#423)
    * rubocop exception
    
    * adds version back to docker compose templates

--- lib/camerata/dot_rc.rb
+++ lib/camerata/dot_rc.rb
@@ -3,12 +3,14 @@ module Camerata
   class DotRc
     include Thor::Shell
 
+    # rubocop:disable Lint/LiteralAsCondition
     def initialize
       @file = find_file
       say_status :load, 'not found', :red && return unless @file
       say_status :load, @file
       load(@file)
     end
+    # rubocop:enable Lint/LiteralAsCondition
 
     def find_file
       path = nil
