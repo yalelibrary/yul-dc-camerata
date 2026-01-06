f1c400c17845523dbb256c768cf4bcbd5503b58e
Keith Boyd-Carter <keith.boyd-carter@yale.edu>
2026-01-06 14:34:50 -0500

    update works min deploy percentage

--- bin/deploy-intensive-worker.sh
+++ bin/deploy-intensive-worker.sh
@@ -41,7 +41,7 @@ then
     --project-name ${CLUSTER_NAME}-intensive-worker \
     --ecs-params ${CLUSTER_NAME}-intensive-params.yml \
     service up \
-    --deployment-min-healthy-percent 50 \
+    --deployment-min-healthy-percent 0 \
     --launch-type FARGATE \
     $2 \
     $discovery $log \
