b72b473d81a7380f924f2c5fa1eae27a382343a9
K8Sewell <kait@notch8.com>
2026-06-29 13:27:40 -0500

    Add and remove variables

--- bin/get-params.sh
+++ bin/get-params.sh
@@ -212,6 +212,8 @@ task_definition:
       secrets:
         - value_from: arn:aws:secretsmanager:us-east-1:106281100175:secret:yul-dcs/demo/iiif_image_keystore:IIIF_IMAGE_KEYSTORE::
           name: IIIF_IMAGE_KEYSTORE
+        - value_from: arn:aws:secretsmanager:us-east-1:106281100175:secret:yul-dcs/demo/iiif_image_keystore:HTTPS_KEY_STORE_PASSWORD
+          name: HTTPS_KEY_STORE_PASSWORD
   ecs_network_mode: awsvpc
   task_size:
     mem_limit: 8192
